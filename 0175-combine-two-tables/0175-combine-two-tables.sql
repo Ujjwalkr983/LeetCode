a18dbe22b993a3b5ea27536bc40366b2155545e5
Ujjwalkr983 <143831102+Ujjwalkr983@users.noreply.github.com>
2024-05-31 14:28:50 +0530

    Time: 675 ms (87.99%), Space: 0B (100%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT firstName, lastName, city, state FROM Person
+LEFT JOIN Address
+ON Person.personId = Address.personId
+WHERE firstName IS NOT NULL
+UNION
+SELECT firstName, lastName, city, state FROM Person
+RIGHT JOIN Address
+ON Person.personId = Address.personId
+WHERE firstName IS NOT NULL;
