3ff17ca11d308a3ec9943e2f258ed4ba2b30fc13
Ujjwalkr983 <143831102+Ujjwalkr983@users.noreply.github.com>
2025-11-26 14:59:05 +0530

    Time: 3 ms (53.79%), Space: 14 MB (90.71%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int target, n;
-    void fun(int ind, int sum, vector<int>& temp, vector<vector<int>> &ans, vector<int>& nums){
-        if(target == sum){ 
-            ans.push_back(temp); return;
-        }
-        if(sum > target || ind >= n) return;
-        for(int i = ind; i<nums.size(); i++){
-            temp.push_back(nums[i]);
-            fun(i, sum + nums[i], temp, ans, nums);
-            temp.pop_back();
-        }
 
+    void fun(int i, int sum, vector<int>& ds, vector<vector<int>>& ans, vector<int>& nums, int target){
+
+        if(sum>target || i>=nums.size()) return;
+        if(sum == target){
+            ans.push_back(ds);
+            return;
+        }
+        for(int ind = i; ind<nums.size(); ind++){
+            ds.push_back(nums[ind]);
+            fun(ind, sum+nums[ind], ds, ans, nums, target);
+            ds.pop_back();
+        }
     }
-    vector<vector<int>> combinationSum(vector<int>& nums, int tar) {
+
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         vector<vector<int>> ans;
-        vector<int> temp;
-        target = tar; n = nums.size();
-        fun(0, 0, temp, ans, nums);
+        vector<int> ds;
+        fun(0, 0, ds, ans, nums, target);
         return ans;
     }
 };
